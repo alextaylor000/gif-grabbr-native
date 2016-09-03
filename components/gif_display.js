4c185c9d7a74ea65b3e5c87b5f99d5322b7a7f0b
Alex Taylor <alexmctaylorpants@gmail.com>
2016-09-02 23:30:12 -0700

    Handle 404

--- components/gif_display.js
+++ components/gif_display.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { TouchableHighlight, Linking, View, Text, Image, ActivityIndicator } from 'react-native';
+const fallbackImage = 'https://media3.giphy.com/media/11gZBGuDnYwdpu/200_d.gif';
+const fallbackImageURL = 'http://giphy.com/gifs/gustavo-art-glitch-image-11gZBGuDnYwdpu';
 
 class GifDisplay extends Component {
   constructor() {
@@ -23,8 +25,8 @@ class GifDisplay extends Component {
   render() {
     const { gif } = this.props;
     if (this.props.prefetch) return this.showSpinner();
-    const siteURL = gif.url;
-    const imageURL = gif.fixed_height_downsampled_url.replace('http:', 'https:');
+    const siteURL = gif.id ? gif.url : fallbackImageURL;
+    const imageURL = gif.id ? gif.fixed_height_downsampled_url.replace('http:', 'https:') : fallbackImage;
     return (
       <TouchableHighlight style={{ flex: 1 }} onPress={() => Linking.openURL(siteURL) }>
         <Image
